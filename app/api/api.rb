339862af6aa7616cdb9862edd2d4b0257e5cbd3d
Aurélien Hervé <mail@aurelien-herve.com>
2014-11-20 17:26:26 +0100

    swagger-doc

--- app/api/api.rb
+++ app/api/api.rb
@@ -3,9 +3,11 @@ require 'grape-swagger'
 class API < Grape::API
   version :v1, using: :accept_version_header, format: :json, default_format: :json do 
 
+    # Mount some api methods
     mount MyApi::V1::Ping
 
-    add_swagger_documentation(mount_path: '/doc/swagger_doc', base_path: '/api')
+    # Generate an api documentation
+    add_swagger_documentation(mount_path: '/doc/swagger_doc', base_path: '/api') 
 
   end
 
