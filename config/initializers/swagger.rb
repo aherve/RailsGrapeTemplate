8e8887b21c617bb3a57dc57b4a385c6c2850289c
Aurélien Hervé <mail@aurelien-herve.com>
2014-11-20 17:28:42 +0100

    swagger ok

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,2 +1,3 @@
-GrapeSwaggerRails.options.url = '/doc/swagger_doc.json'
-GrapeSwaggerRails.options.app_url = 'http://localhost:3000/api'
+GrapeSwaggerRails.options.url = '/api/doc/swagger_doc.json'
+GrapeSwaggerRails.options.app_url = 'http://localhost:3000'
+GrapeSwaggerRails.options.app_name = 'RailsGrapeTemplate'
